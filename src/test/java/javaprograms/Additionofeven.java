de69dbc081c5ff462b36e50993514f10745eb346
Shubhammohite0007 <124283975+Shubhammohite0007@users.noreply.github.com>
2023-12-18 18:58:00 +0530

    Update Additionofeven.java

--- src/test/java/javaprograms/Additionofeven.java
+++ src/test/java/javaprograms/Additionofeven.java
@@ -4,7 +4,7 @@ public class Additionofeven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=14;
+		int num=16;
 		int sum=0;
 		
 		for(int i=1;i<=num;i++) {
